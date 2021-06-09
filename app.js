4e22cb452f5c32c39e2044e530ab149e772a39c3
AlanGMerkin <alangmerkin@gmail.com>
2021-06-08 21:30:31 -0300

    Mercado-Liebre

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/login', (req,res) =>{
+    res.sendFile((__dirname + '/views/login.html')); 
+});
+
+app.get('/register', (req,res) =>{
+    res.sendFile((__dirname + '/views/register.html')); 
+});
+
+app.get('/mercado_liebre', (req,res) =>{
+    res.sendFile((__dirname + '/views/home.html'));
+});
+
+app.use(express.static(__dirname +'/public'));  
+
+app.listen(process.env.PORT || 3030, () => {
+    console.log("Servidor corriendo");
+});
+
+
+
